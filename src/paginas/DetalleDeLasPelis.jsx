9f46cc24765748ba716a69b18fdf07fad2411fb2
Gonzalo Cubillas <gonzalo.cubillas15@gmail.com>
2021-10-26 13:02:39 -0300

    seguimos practicando

--- src/paginas/DetalleDeLasPelis.jsx
+++ src/paginas/DetalleDeLasPelis.jsx
@@ -1,5 +1,25 @@
+import listita from './Movie.json';
+import style from './Detalles.module.css'
+
+
 export function DetalleDeLasPelis (){
+    const imagenURL = "https://image.tmdb.org/t/p/w500" + listita.poster_path;
     return(
-        <h3>Detalles de la pelicula</h3>
+        <div className={style.detalleMovie}>
+            <img src={imagenURL} alt={listita.name} className={` ${style.col} ${style.imagen}`} />
+            <div className={`${style.col} ${style.texto}`}> 
+            <p><strong>Titulo:</strong>  {listita.original_title}</p>
+         
+         
+                <p><strong>Genero:</strong>{(' ')}
+                {
+                    listita.genres.map(genero =>(genero.name)).join(', ')
+                }
+                </p>
+            
+          
+                <p><strong>Descripcion:</strong> {listita.overview}</p>
+            </div>
+        </div>
     )
 }
